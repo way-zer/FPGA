5a51fdf35c38da0a83c019ab3d9472364b6f41a5
way-zer <q1048203787@gmail.com>
2020-11-01 20:50:26 +0800

    [Update] 升级数码显示到16位

--- Quartus/source/utils/Num.v
+++ Quartus/source/utils/Num.v
@@ -3,16 +3,22 @@ module Num(
 	output reg [0:7] segO
 );
 	always@(numI) case(numI)
-		0: segO <= 8'b1111_1100;
-		1: segO <= 8'b0110_0000;
-		2: segO <= 8'b1101_1010;
-		3: segO <= 8'b1111_0010;
-		4: segO <= 8'b0110_0110;
-		5: segO <= 8'b1011_0110;
-		6: segO <= 8'b1011_1110;
-		7: segO <= 8'b1110_0000;
-		8: segO <= 8'b1111_1110;
-		9: segO <= 8'b1111_0110;
+		4'h0: segO <= 8'b1111_1100;
+		4'h1: segO <= 8'b0110_0000;
+		4'h2: segO <= 8'b1101_1010;
+		4'h3: segO <= 8'b1111_0010;
+		4'h4: segO <= 8'b0110_0110;
+		4'h5: segO <= 8'b1011_0110;
+		4'h6: segO <= 8'b1011_1110;
+		4'h7: segO <= 8'b1110_0000;
+		4'h8: segO <= 8'b1111_1110;
+		4'h9: segO <= 8'b1111_0110;
+		4'ha: segO <= 8'b1110_1110;
+		4'hb: segO <= 8'b0011_1110;
+		4'hc: segO <= 8'b1001_1100;
+		4'hd: segO <= 8'b0111_1010;
+		4'he: segO <= 8'b1001_1110;
+		4'hf: segO <= 8'b1000_1110;
 		default: segO= 0;
 	endcase
 endmodule
