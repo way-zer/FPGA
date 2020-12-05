21f9c60a7f7f011527464b1e49658665dceaab1a
way-zer <q1048203787@gmail.com>
2020-12-05 19:49:01 +0800

    [Update](FinalExpr)升级蜂鸣器音频模块，支持21音

--- FinalExpr/source/module/Music.v
+++ FinalExpr/source/module/Music.v
@@ -1,27 +1,28 @@
 module Music (
     input clk,en,
-    input [2:0] scale,//音阶数 0-6
+    input [4:0] scale,//音阶数 1-21,0为空音
     input [10:0] playTime,//持续ms数<2048
     output finish,
     output beep
 );
     parameter BaseFreq = 1_000_000;
     // 各音阶周期，最大为3831
-    parameter [0:32*7-1] ClkM = {BaseFreq/261,BaseFreq/294,BaseFreq/330,BaseFreq/349,BaseFreq/392,BaseFreq/440,BaseFreq/494};
-    wire [6:0] clks;//各音符时钟
-    generate 
-        genvar genI;
-        for (genI = 0; genI<7; genI=genI+1) begin:clkBlock
-            DivideClk#(ClkM[genI*32+:32],ClkM[genI*32+:32]/2) u_DivideClk(clk,en,clks[genI]);
-        end
-    endgenerate
-    assign beep = en&clks[scale];
+    parameter [0:32*7*3-1] ClkM = {
+        BaseFreq/261,BaseFreq/294,BaseFreq/330,BaseFreq/349,BaseFreq/392,BaseFreq/440,BaseFreq/494,//低音
+        BaseFreq/523,BaseFreq/587,BaseFreq/659,BaseFreq/698,BaseFreq/784,BaseFreq/880,BaseFreq/988,//中音
+        BaseFreq/1047,BaseFreq/1175,BaseFreq/1319,BaseFreq/1397,BaseFreq/1568,BaseFreq/1760,BaseFreq/1096//高音
+    };
+    wire [11:0] M=ClkM[(scale-1)*32+:32];
+    wire clkO;
+    DivideClkMN#(12) u_DivideClkMN(clk,en,M,M>>1,clkO);
+    assign beep = |scale?(en&clkO):1'b0;
     
     DivideClk#(BaseFreq/1000,BaseFreq/1000) u0(clk,en,clk1K);
-    reg [10:0] cntT;
+    reg [10:0] cntT=0;
     always @(posedge clk1K or negedge en) begin
         if(!en)cntT<=0;
+        else if(finish) cntT<=0;
         else cntT <= cntT+1;
     end
-    assign finish = cntT>playTime;
+    assign finish = cntT>=playTime;
 endmodule
--- FinalExpr/source/test/scripts/music.do
+++ FinalExpr/source/test/scripts/music.do
@@ -2,16 +2,16 @@ set Module Music
 do pre.do
 
 #调整变量显示方式
-radix signal /$Module/i u
-radix signal /$Module/t u
+radix signal /$Module/scale u
+radix signal /$Module/playTime u
 
 #修改分频器时长,降低仿真时间
-change /$Module/BaseFreq 32'd10000
+change /$Module/BaseFreq 32'd100000
 
 force /$Module/clk 0 0,1 50ns -r 100ns
 force /$Module/en 0 0,1 5000ns
-force /$Module/i 0
-force /$Module/t 1000
+force /$Module/scale 1
+force /$Module/playTime 1000
 
 run 1ms
 
